ac13da986aae54367c74ff80b86648258cc38f0d
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-08 05:51:49 -0700

    Keep working on July 08, 2024

--- app.js
+++ app.js
@@ -3,13 +3,38 @@ const user = {
     password
 }
 
+const admin = {
+    email: 'usamaisrar1189@gmail.com',
+    password: 'usama1189'
+}
+
+sessionStorage.setItem('admin', JSON.stringify(admin));
+
+let emailAlreadyExits = JSON.parse(sessionStorage.getItem('user'));
+
+console.log(emailAlreadyExits);
+
+let warning = document.getElementById('demo');
+
+const signup = document.getElementById('signup');
 
 
-let signup = document.getElementById('signup');
-
-signup.addEventListener('click', () => {
+signup.addEventListener('click', (e) => {
+    e.preventDefault();
+    
     user.email = document.getElementById('email').value;
     user.password = document.getElementById('password').value;
 
-    sessionStorage.setItem('user', user);
+    if(user.email === emailAlreadyExits.email) {
+        warning.innerHTML = 'This email address already exits';
+    }
+    else if(user.email === admin.email) {}
+    
+    else {
+        warning.innerHTML = 'This email address is not registered';
+        sessionStorage.setItem('user', JSON.stringify(user));
+
+        document.getElementById('signupForm').style.display = 'none';
+        console.log(emailAlreadyExits);
+    }
 });
