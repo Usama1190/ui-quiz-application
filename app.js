67cda70f7725a5cd20d6f7705ddaf36a5940e439
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-09 04:44:35 -0700

    Keep working on July 09, 2024

--- app.js
+++ app.js
@@ -118,3 +118,55 @@ typescript.addEventListener('click', (e) => {
     quizinfo_wrapper.style.display = 'none';
     quizguide_wrapper.style.display = 'block';
 })
+
+
+const start = document.getElementById('start');
+let quizdashboard_wrapper = document.getElementById('quizdashboard_wrapper');
+
+let quizQuestions = [
+    "What are the three main 'simple types' in TypeScript?",
+    "What type of assignment is this variable, `const fullName: string = 'Dylan Israel';`?",
+    "True or False: TypeScript can always correctly infer a variables type.",
+    "You can disable implicit variable type assignment by enabling the compiler option:",
+    "You can enable 'undefined' and 'null' types to be accounted for by enabling the compiler property:",
+    "______ is similar to 'any', but a safer alternative when uncertain about the type.",
+    "What is the inherited type for the variable example in `const example = ['Dylan']`?",
+    "What does the 'readonly' access modifier do for an array variable assignment like: `const codeNames: readonly string[] = ['Coding', 'God']`?",
+    "True or False: TypeScript will always correctly infer the type of an array.",
+    "True or False: a Tuple and an Array are the same thing when discussing types",
+    "Which is a successful example of this tuple `[number, string]`?",
+    "Type Aliases are mostly used with ______.",
+    "True or False: Interfaces are similar to type aliases, but only for object types?",
+    "________ an interface will have the same properties as that interface.",
+    "What are the two types of enums?",
+    "Numeric enums first value is defaulted to what?",
+    "True or False: 'keyof' can be used with index signatures to extract the index type.",
+    "True or False: Generics can not be assigned default values.",
+    "Definitely Typed is...",
+    "What is the type of the parameter: `function ex(param1?: string){}`?",
+    "_____ is a return type for when nothing is returned.",
+    "Access modifiers control the ______ of properties and methods.",
+    "True or False: public modifiers allow access to the class members from anywhere.",
+    "True or False: protected modifiers only are allowed in the inherited class.",
+    "When a class extends another class and replaces the members of its parent it is called what?"
+]
+
+start.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    quizguide_wrapper.style.display = 'none';
+    quizdashboard_wrapper.style.display = 'block';
+
+    for(let i = 0; i < quizQuestions.length; i++) {
+        let question = document.createElement('div');
+        question.classList.add('question');
+        question.innerHTML = quizQuestions[i];
+        quizdashboard_wrapper.appendChild(question);
+
+        // let nextBtn = document.createElement('button');
+        // let nextBtnText = document.createTextNode('Next');
+        // nextBtn.appendChild(nextBtnText);
+        // nextBtn.classList.add('btn');
+        // quizdashboard_wrapper.appendChild(nextBtn);
+    }
+})
