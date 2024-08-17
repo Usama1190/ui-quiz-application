afd2d5368ac3feb6932afff31d1bb2d7e1d7e995
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-17 05:19:44 -0700

    Updating Quiz Application on Aug 17, 24

---
+++ firebase/app.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ZjF4ezJc5sUh0FyfLiDKUTJ-YrffAIY",
+    authDomain: "quiz-app-9e838.firebaseapp.com",
+    projectId: "quiz-app-9e838",
+    storageBucket: "quiz-app-9e838.appspot.com",
+    messagingSenderId: "547753696299",
+    appId: "1:547753696299:web:26d1a05e87e36b2de85e6e",
+    measurementId: "G-9JRLQD0WTT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+export { getAuth };
